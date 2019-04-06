c3ecbf0f1982b01eadbb7b10ef21fdc3c6134a13
Abner Rodrigues <abnerodrigs@gmail.com>
2019-04-05 21:18:27 -0300

    first version

---
+++ src/com/kingaspx/menu/Menu.java
@@ -0,0 +1,130 @@
+package com.kingaspx.menu;
+
+import com.kingaspx.util.AbrirSite;
+
+public class Menu extends javax.swing.JFrame {
+
+    AbrirSite site = new AbrirSite();
+
+    public Menu() {
+        initComponents();
+        site.abrirPanel("google.com", jPanel3, jLabel1);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(102, 173, 199));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/kingaspx/icon/icons8_Delete_20px_7.png"))); // NOI18N
+        jButton1.setToolTipText("Fechar");
+        jButton1.setContentAreaFilled(false);
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.setFocusPainted(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 0, -1, 30));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/kingaspx/icon/icons8_Globe_20px_4.png"))); // NOI18N
+        jLabel1.setText("Título Aqui");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, -1, 30));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 30));
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanel2.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 40, 800, 530));
+        jPanel2.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 730, -1));
+
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/kingaspx/icon/icons8_Search_20px_5.png"))); // NOI18N
+        jButton2.setToolTipText("Ir");
+        jButton2.setContentAreaFilled(false);
+        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton2.setFocusPainted(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 0, -1, 40));
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 30, 800, 570));
+
+        setSize(new java.awt.Dimension(800, 600));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        site.browser.loadURL(jTextField1.getText());
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Menu().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/com/kingaspx/util/AbrirSite.java
@@ -0,0 +1,90 @@
+package com.kingaspx.util;
+
+import com.kingaspx.version.Version;
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.BrowserContext;
+import com.teamdev.jxbrowser.chromium.BrowserContextParams;
+import com.teamdev.jxbrowser.chromium.BrowserPreferences;
+import com.teamdev.jxbrowser.chromium.events.ConsoleEvent;
+import com.teamdev.jxbrowser.chromium.events.TitleEvent;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class AbrirSite {
+
+    public Browser browser;
+    public BrowserView view;
+
+    public void abrirFrame(String URL) {
+        String perfil = "C:\\Historico Chromium\\KingAspx\\";
+
+        //Licença do JX
+        BrowserUtil.setVersion(Version.V6_22);
+
+        BrowserContextParams params = new BrowserContextParams(perfil);
+        BrowserPreferences.setChromiumSwitches("user-data-dir=" + perfil);
+        BrowserContext context = new BrowserContext(params);
+
+        browser = new Browser(context);
+        view = new BrowserView(browser);
+
+        JFrame frame = new JFrame();
+        frame.add(view, BorderLayout.CENTER);
+        frame.setSize(800, 600); //seta o tamanho que o jframe vai iniciar
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setExtendedState(6);
+        frame.setUndecorated(true);
+        frame.setVisible(true);
+
+        //Mudar título do JFrame automaticamente
+        browser.addTitleListener((TitleEvent evt) -> {
+            frame.setTitle(evt.getTitle());
+        });
+
+        //Pegar o erro do console
+        browser.addConsoleListener((ConsoleEvent evt) -> {
+            System.out.println("LOG: " + evt.getMessage());
+        });
+
+        browser.loadHTML(URL);
+    }
+
+    public void abrirPanel(String URL, JPanel panel, JLabel label) {
+        //Se estiver comentado = Sem perfil
+        //Se não estiver = Com perfil
+        String perfil = "C:\\Historico Chromium\\KingAspx\\";
+
+        //Licença do JX
+        BrowserUtil.setVersion(Version.V6_22);
+
+        BrowserContextParams params = new BrowserContextParams(perfil);
+        BrowserPreferences.setChromiumSwitches("user-data-dir=" + perfil);
+        BrowserContext context = new BrowserContext(params);
+        browser = new Browser(context);
+        view = new BrowserView(browser);
+
+        panel.add(view, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 530));
+
+        //Mudar título do JFrame automaticamente
+        browser.addTitleListener((TitleEvent evt) -> {
+            label.setText(evt.getTitle());
+        });
+
+        //Pegar o erro do console
+        browser.addConsoleListener((ConsoleEvent evt) -> {
+            System.out.println("LOG: " + evt.getMessage());
+        });
+
+        browser.loadURL(URL);
+    }
+
+    public static void main(String[] args) {
+        new AbrirSite().abrirFrame("google.com");
+    }
+
+}
